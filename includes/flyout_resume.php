11c57a977a32f281fbc8915a35a57b4378ea500a
Jason Grant <mrjaygee@gmail.com>
2013-10-01 13:56:29 -0600

    trying to add and remove files

--- includes/flyout_resume.php
+++ includes/flyout_resume.php
@@ -2,20 +2,16 @@
 	<div class="arrow"></div>
 	<h4>Résumé Papers - <span>Best for résumés and executive correspondence.</span></h4>
 		<div id="tiles">
-		<div class="tile"><div class="swatch white">&nbsp;</div> Gloss Text <img src="img/recycle-icon.png" alt="" /></div>
-		<div class="tile"><div class="swatch white">&nbsp;</div> Standard White <img src="img/recycle-icon.png" alt="" /></div>	
-		<div class="tile"><div class="swatch ivory">&nbsp;</div>Ivory <img src="img/recycle-icon.png" alt="" /></div>
-		<div class="tile"><div class="swatch canary">&nbsp;</div>Canary <img src="img/recycle-icon.png" alt="" /></div>
-		<div class="tile"><div class="swatch salmon">&nbsp;</div>Salmon <img src="img/recycle-icon.png" alt="" /></div>
-		<div class="tile"><div class="swatch orchid">&nbsp;</div>Orchid <img src="img/recycle-icon.png" alt="" /></div>			
-		<div class="tile"><div class="swatch pastel_blue">&nbsp;</div>Pastel Blue <img src="img/recycle-icon.png" alt="" /></div>
-		<div class="tile"><div class="swatch green">&nbsp;</div>Green <img src="img/recycle-icon.png" alt="" /></div>
-		<div class="tile"><div class="swatch ultra_lemon">&nbsp;</div>Ultra Lemon</div>
-		<div class="tile"><div class="swatch sun_yellow">&nbsp;</div>Sun Yellow</div>	
-		<div class="tile"><div class="swatch ultra_orange">&nbsp;</div>Ultra Orange <img src="img/recycle-icon.png" alt="" /></div>
-		<div class="tile"><div class="swatch red">&nbsp;</div>Red <img src="img/recycle-icon.png" alt="" /></div>
-		<div class="tile"><div class="swatch ultra_fuschia">&nbsp;</div>Ultra Fuschia <img src="img/recycle-icon.png" alt="" /></div>			
-		<div class="tile"><div class="swatch bright_blue">&nbsp;</div>Bright Blue <img src="img/recycle-icon.png" alt="" /></div>
-		<div class="tile"><div class="swatch ultra_lime">&nbsp;</div>Ultra Lime <img src="img/recycle-icon.png" alt="" /></div>
+		<div class="tile"><div class="swatch white">&nbsp;</div> Ultra Bright White</div>
+		<div class="tile"><div class="swatch white">&nbsp;</div> Pure White</div>
+		
+		<div class="tile"><div class="swatch natural">&nbsp;</div> Natural <img src="img/recycle-icon.png" alt="" /></div>
+		<div class="tile"><div class="swatch sand_stone">&nbsp;</div> Sand Stone <img src="img/recycle-icon.png" alt="" /></div>	
+		<div class="tile"><div class="swatch sea_spray">&nbsp;</div>Sea Spray <img src="img/recycle-icon.png" alt="" /></div>
+		<div class="tile"><div class="swatch willow">&nbsp;</div>Willow <img src="img/recycle-icon.png" alt="" /></div>
+		<div class="tile"><div class="swatch antique_parchment">&nbsp;</div>Antique Parchment <img src="img/recycle-icon.png" alt="" /></div>
+		<div class="tile"><div class="swatch tan">&nbsp;</div>Tan <img src="img/recycle-icon.png" alt="" /></div>
+		<div class="tile"><div class="swatch antique_gray">&nbsp;</div>Antique Gray <img src="img/recycle-icon.png" alt="" /></div>
+		<div class="tile"><div class="swatch soft_pink">&nbsp;</div>Soft Pink <img src="img/recycle-icon.png" alt="" /></div>
 	</div>
 </div>
