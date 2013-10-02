7200fff98e819d6ff20c51ac3840ee6be56eedd5
Jason Grant <mrjaygee@gmail.com>
2013-10-02 15:28:08 -0600

    fixed all flyouts with recycled class

--- includes/flyout_cover.php
+++ includes/flyout_cover.php
@@ -6,22 +6,22 @@
 		<div class="tile"><div class="swatch white">&nbsp;</div> 110lb. Index</div>
 		<div class="tile"><div class="swatch white">&nbsp;</div> Laser <small>80lb.</small></div>
 		<div class="tile"><div class="swatch white">&nbsp;</div> Gloss Cover</div>
-		<div class="tile"><div class="swatch natural">&nbsp;</div> Natural <img src="img/recycle-icon-gray.png" /></div>
-		<div class="tile"><div class="swatch sand_stone">&nbsp;</div> Sand Stone <img src="img/recycle-icon-gray.png" /></div>	
-		<div class="tile"><div class="swatch sea_spray">&nbsp;</div>Sea Spray <img src="img/recycle-icon-gray.png" /></div>
-		<div class="tile"><div class="swatch antique_gray">&nbsp;</div>Antique Gray <img src="img/recycle-icon-gray.png" /></div>
-		<div class="tile"><div class="swatch soft_pink">&nbsp;</div>Soft Pink <img src="img/recycle-icon-gray.png" /></div>
-		<div class="tile"><div class="swatch ivory">&nbsp;</div>Ivory <img src="img/recycle-icon-gray.png" /></div>		
-		<div class="tile"><div class="swatch canary">&nbsp;</div>Canary <img src="img/recycle-icon-gray.png" /></div>
-		<div class="tile"><div class="swatch pastel_blue">&nbsp;</div>Pastel Blue <img src="img/recycle-icon-gray.png" /></div>
-		<div class="tile"><div class="swatch green">&nbsp;</div>Green <img src="img/recycle-icon-gray.png" /></div>
+		<div class="tile recycled"><div class="swatch natural">&nbsp;</div> Natural</div>
+		<div class="tile recycled"><div class="swatch sand_stone">&nbsp;</div> Sand Stone</div>	
+		<div class="tile recycled"><div class="swatch sea_spray">&nbsp;</div>Sea Spray</div>
+		<div class="tile recycled"><div class="swatch antique_gray">&nbsp;</div>Antique Gray</div>
+		<div class="tile recycled"><div class="swatch soft_pink">&nbsp;</div>Soft Pink</div>
+		<div class="tile recycled"><div class="swatch ivory">&nbsp;</div>Ivory</div>		
+		<div class="tile recycled"><div class="swatch canary">&nbsp;</div>Canary</div>
+		<div class="tile recycled"><div class="swatch pastel_blue">&nbsp;</div>Pastel Blue</div>
+		<div class="tile recycled"><div class="swatch green">&nbsp;</div>Green</div>
 		<div class="tile"><div class="swatch ultra_lemon">&nbsp;</div>Ultra Lemon</div>
 		<div class="tile"><div class="swatch sun_yellow">&nbsp;</div>Sun Yellow</div>	
-		<div class="tile"><div class="swatch ultra_orange">&nbsp;</div>Ultra Orange <img src="img/recycle-icon-gray.png" /></div>
-		<div class="tile"><div class="swatch red">&nbsp;</div>Red <img src="img/recycle-icon-gray.png" /></div>
-		<div class="tile"><div class="swatch ultra_fuschia">&nbsp;</div>Ultra Fuschia <img src="img/recycle-icon-gray.png" /></div>			
-		<div class="tile"><div class="swatch bright_blue">&nbsp;</div>Bright Blue <img src="img/recycle-icon-gray.png" /></div>
-		<div class="tile"><div class="swatch ultra_lime">&nbsp;</div>Ultra Lime <img src="img/recycle-icon-gray.png" /></div>
+		<div class="tile recycled"><div class="swatch ultra_orange">&nbsp;</div>Ultra Orange</div>
+		<div class="tile recycled"><div class="swatch red">&nbsp;</div>Red</div>
+		<div class="tile recycled"><div class="swatch ultra_fuschia">&nbsp;</div>Ultra Fuschia</div>			
+		<div class="tile recycled"><div class="swatch bright_blue">&nbsp;</div>Bright Blue</div>
+		<div class="tile recycled"><div class="swatch ultra_lime">&nbsp;</div>Ultra Lime</div>
 	</div>
 	<legend>
 		<img src="img/recycle-icon-gray.png" alt="" /> = Recycled Content
--- includes/flyout_resume.php
+++ includes/flyout_resume.php
@@ -5,14 +5,14 @@
 		<div class="tile"><div class="swatch white">&nbsp;</div> Ultra Bright White</div>
 		<div class="tile"><div class="swatch white">&nbsp;</div> Pure White</div>
 		
-		<div class="tile"><div class="swatch natural">&nbsp;</div> Natural <img src="img/recycle-icon-gray.png" /></div>
-		<div class="tile"><div class="swatch sand_stone">&nbsp;</div> Sand Stone <img src="img/recycle-icon-gray.png" /></div>	
-		<div class="tile"><div class="swatch sea_spray">&nbsp;</div>Sea Spray <img src="img/recycle-icon-gray.png" /></div>
-		<div class="tile"><div class="swatch willow">&nbsp;</div>Willow <img src="img/recycle-icon-gray.png" /></div>
-		<div class="tile"><div class="swatch antique_parchment">&nbsp;</div>Antique Parchment <img src="img/recycle-icon-gray.png" /></div>
-		<div class="tile"><div class="swatch tan">&nbsp;</div>Tan <img src="img/recycle-icon-gray.png" /></div>
-		<div class="tile"><div class="swatch antique_gray">&nbsp;</div>Antique Gray <img src="img/recycle-icon-gray.png" /></div>
-		<div class="tile"><div class="swatch soft_pink">&nbsp;</div>Soft Pink <img src="img/recycle-icon-gray.png" /></div>
+		<div class="tile recycled"><div class="swatch natural">&nbsp;</div> Natural</div>
+		<div class="tile recycled"><div class="swatch sand_stone">&nbsp;</div> Sand Stone</div>	
+		<div class="tile recycled"><div class="swatch sea_spray">&nbsp;</div>Sea Spray</div>
+		<div class="tile recycled"><div class="swatch willow">&nbsp;</div>Willow</div>
+		<div class="tile recycled"><div class="swatch antique_parchment">&nbsp;</div>Antique Parchment</div>
+		<div class="tile recycled"><div class="swatch tan">&nbsp;</div>Tan</div>
+		<div class="tile recycled"><div class="swatch antique_gray">&nbsp;</div>Antique Gray</div>
+		<div class="tile recycled"><div class="swatch soft_pink">&nbsp;</div>Soft Pink</div>
 	</div>
 	<legend>
 		<img src="img/recycle-icon-gray.png" /> = Recycled Content
