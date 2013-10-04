804e59b960dbfdadebe5e5e36bc73b67b02872f8
Jason Grant <mrjaygee@gmail.com>
2013-10-04 08:41:02 -0600

    tooltips

--- includes/upload_area.php
+++ includes/upload_area.php
@@ -1,22 +1,29 @@
 <div id="print_preview">
 	<div id="fileupload-container">
 		<h2>Drag &amp; Drop your file here <img src="img/fxo_upload2.png" alt="Upload" /></h2>
-		<div class="hero-unit fxo-fileupload-methodbox-narrow">
+		<div class="hero-unit">
 	        <div class="fileupload-or-container text-center"> OR</div>
             <div class="fileupload-splitbox fileupload-splitbox-50">
                     <button class="mybutton">Select From Computer</button>
-                    <div class="fxo-fileupload-pdfrecommended"><small>PDFs are recommended</small></div>
+                    <div class="fileupload-pdfrecommended"><small>PDFs are recommended</small></div>
             </div>
 		
             <div class="fileupload-splitbox fileupload-splitbox-50">
-                <div class="fxo-fileupload-inputbox">
-                    <button class="btn fxo-fileupload-cloudbutton"><div class="fxo-fileupload-icon-fxo"></div></button>
-                    <button class="btn fxo-fileupload-cloudbutton"><div class="fxo-fileupload-icon-box"></div></button>
-                    <button class="btn fxo-fileupload-cloudbutton"><div class="fxo-fileupload-icon-dropbox"></div></button>
-                    <button class="btn fxo-fileupload-cloudbutton"><div class="fxo-fileupload-icon-googledrive"></div></button>
+                <div class="fileupload-inputbox">
+                    <button class="btn fileupload-cloudbutton"><div class="fileupload-icon-fxo"></div></button>
+                    <button class="btn fileupload-cloudbutton"><div class="fileupload-icon-box"></div></button>
+                    <button class="btn fileupload-cloudbutton"><div class="fileupload-icon-dropbox"></div></button>
+                    <button class="btn fileupload-cloudbutton"><div class="fileupload-icon-googledrive"></div></button>
+                    
+                    <div id="fxo" class="tooltip">FedEx Office</div>
+                    <div id="box" class="tooltip">Box</div>
+                    <div id="dropbox" class="tooltip">Dropbox</div>
+                    <div id="googledrive" class="tooltip">Google Drive</div>
+                	<!--<ul>
+                		<li>FedEx Office</li><li>&nbsp;&nbsp;&nbsp;Box&nbsp;&nbsp;&nbsp;</li><li>Dropbox</li><li>Google Drive</li>
+                	</ul>-->
                 </div>
-
-                <div class="fxo-fileupload-cloudtransfer"><small><strong>Transfer from Cloud Service</strong></small></div>
+                <div class="fileupload-cloudtransfer"><small><strong>Transfer from Cloud Service</strong></small></div>
             </div>
 		    <div class="clearfix"></div>
 		</div>
