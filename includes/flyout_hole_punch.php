058b88344851843f09b91aee40b9ee616337d608
Jason Grant <mrjaygee@gmail.com>
2013-10-06 19:51:29 -0600

    modified finishing options

--- includes/flyout_hole_punch.php
+++ includes/flyout_hole_punch.php
@@ -1,6 +1,6 @@
 <div id="flyout_hole_punching" class="flyouts">
 	<div class="close"><img src="img/x-button-icon.png"></div>
-	<h4>Hole Punching - <span>For putting your documents in a binder.</span></h4>
+	<h4>Hole Punching Options<span></span></h4>
 	<div id="tiles">
 		<div class="tile selected-option"><img src="img/hole-punch-no-hole.png" alt="No Hole Punch" /><br>No Hole Punch</div>
 		<div class="tile"><img src="img/hole-punch-02-hole.png" alt="2-Hole Punch" /><br>2 Hole Punch (side)</div>
