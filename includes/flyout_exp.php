99bd737305ac52df6664ebf4d36f6c78fa5fe515
Jason Grant <mrjaygee@gmail.com>
2013-10-09 11:43:57 -0600

    starting experimental paper picker

---
+++ includes/flyout_exp.php
@@ -0,0 +1,79 @@
+<div id="flyout_tabbed" class="flyouts">
+	<div class="close"><img src="img/x-button-icon.png"></div>
+	<h4>Paper Options</h4>
+	<div id="paper_types">
+		<ul>
+			<li id="general" class="selected">General Use Papers<br>
+			<p>Good for your everyday document printing needs.</p></li>
+			<li id="resume">Résumé Papers<br>
+			<p>Best for résumés and executive correspondence.</p></li>
+			<li id="cover">Card &amp; Cover Stock<br>
+			<p>For business cards and report covers.</p></li>
+			<li>Transparencies<br>
+			<p>For overhead projections.</p></li>
+		</ul>
+	</div>
+	<div id="tiles">
+		<div id="general">
+			<div class="tile selected-option"><div class="swatch white selected-option">&nbsp;</div> Laser <small>(24lb)</small> </div>
+			<div class="tile"><div class="swatch white">&nbsp;</div> Laser <small>(32lb)</small> </div>
+			<div class="tile"><div class="swatch white">&nbsp;</div> Laser <small>(60lb)</small> </div>
+			<div class="tile"><div class="swatch white">&nbsp;</div> Laser Recycled <small>(24lb)</small></div>
+			<div class="tile recycled"><div class="swatch white">&nbsp;</div> 30% Recycled</div>
+			<div class="tile recycled"><div class="swatch white">&nbsp;</div> 100% Recycled </div>
+			<div class="tile recycled"><div class="swatch white">&nbsp;</div> Gloss Text</div>
+			<div class="tile recycled"><div class="swatch white">&nbsp;</div> Standard White</div>	
+			<div class="tile recycled"><div class="swatch ivory">&nbsp;</div>Ivory</div>
+			<div class="tile recycled"><div class="swatch canary">&nbsp;</div>Canary</div>
+			<div class="tile recycled"><div class="swatch salmon">&nbsp;</div>Salmon</div>
+			<div class="tile recycled"><div class="swatch orchid">&nbsp;</div>Orchid</div>			
+			<div class="tile recycled"><div class="swatch pastel_blue">&nbsp;</div>Pastel Blue</div>
+			<div class="tile recycled"><div class="swatch green">&nbsp;</div>Green</div>
+			<div class="tile"><div class="swatch ultra_lemon">&nbsp;</div>Ultra Lemon</div>
+			<div class="tile"><div class="swatch sun_yellow">&nbsp;</div>Sun Yellow</div>	
+			<div class="tile recycled"><div class="swatch ultra_orange">&nbsp;</div>Ultra Orange</div>
+			<div class="tile recycled"><div class="swatch red">&nbsp;</div>Red</div>
+			<div class="tile recycled"><div class="swatch ultra_fuschia">&nbsp;</div>Ultra Fuschia</div>			
+			<div class="tile recycled"><div class="swatch bright_blue">&nbsp;</div>Bright Blue</div>
+			<div class="tile recycled"><div class="swatch ultra_lime">&nbsp;</div>Ultra Lime</div>
+		</div>
+		<div id="resume" style="display: none;">
+			<div class="tile"><div class="swatch white">&nbsp;</div> Ultra Bright White</div>
+			<div class="tile"><div class="swatch white">&nbsp;</div> Pure White</div>
+			
+			<div class="tile recycled"><div class="swatch natural">&nbsp;</div> Natural</div>
+			<div class="tile recycled"><div class="swatch sand_stone">&nbsp;</div> Sand Stone</div>	
+			<div class="tile recycled"><div class="swatch sea_spray">&nbsp;</div>Sea Spray</div>
+			<div class="tile recycled"><div class="swatch willow">&nbsp;</div>Willow</div>
+			<div class="tile recycled"><div class="swatch antique_parchment">&nbsp;</div>Antique Parchment</div>
+			<div class="tile recycled"><div class="swatch tan">&nbsp;</div>Tan</div>
+			<div class="tile recycled"><div class="swatch antique_gray">&nbsp;</div>Antique Gray</div>
+			<div class="tile recycled"><div class="swatch soft_pink">&nbsp;</div>Soft Pink</div>
+		</div>	
+		<div id="cover" style="display: none;">
+			<div class="tile"><div class="swatch white">&nbsp;</div> Ultra Bright White</div>
+			<div class="tile"><div class="swatch white">&nbsp;</div> 110lb. Index</div>
+			<div class="tile"><div class="swatch white">&nbsp;</div> Laser <small>80lb.</small></div>
+			<div class="tile"><div class="swatch white">&nbsp;</div> Gloss Cover</div>
+			<div class="tile recycled"><div class="swatch natural">&nbsp;</div> Natural</div>
+			<div class="tile recycled"><div class="swatch sand_stone">&nbsp;</div> Sand Stone</div>	
+			<div class="tile recycled"><div class="swatch sea_spray">&nbsp;</div>Sea Spray</div>
+			<div class="tile recycled"><div class="swatch antique_gray">&nbsp;</div>Antique Gray</div>
+			<div class="tile recycled"><div class="swatch soft_pink">&nbsp;</div>Soft Pink</div>
+			<div class="tile recycled"><div class="swatch ivory">&nbsp;</div>Ivory</div>		
+			<div class="tile recycled"><div class="swatch canary">&nbsp;</div>Canary</div>
+			<div class="tile recycled"><div class="swatch pastel_blue">&nbsp;</div>Pastel Blue</div>
+			<div class="tile recycled"><div class="swatch green">&nbsp;</div>Green</div>
+			<div class="tile"><div class="swatch ultra_lemon">&nbsp;</div>Ultra Lemon</div>
+			<div class="tile"><div class="swatch sun_yellow">&nbsp;</div>Sun Yellow</div>	
+			<div class="tile recycled"><div class="swatch ultra_orange">&nbsp;</div>Ultra Orange</div>
+			<div class="tile recycled"><div class="swatch red">&nbsp;</div>Red</div>
+			<div class="tile recycled"><div class="swatch ultra_fuschia">&nbsp;</div>Ultra Fuschia</div>			
+			<div class="tile recycled"><div class="swatch bright_blue">&nbsp;</div>Bright Blue</div>
+			<div class="tile recycled"><div class="swatch ultra_lime">&nbsp;</div>Ultra Lime</div>
+		</div>
+	</div>
+	<legend>
+		<img src="img/recycle-icon-gray.png" /> = Recycled Content
+	</legend>
+</div>
