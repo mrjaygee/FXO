317d26966222d3d011b45e7eab53b0feb08c9c5c
Jason Grant <mrjaygee@gmail.com>
2013-10-09 15:31:13 -0600

    adding lots of stuff for experimental paper picker

--- includes/buttons_exp.php
+++ includes/buttons_exp.php
@@ -1,9 +1,6 @@
-<div id="options" class="popular">
+<div id="options" class="exp">
 	<div class="row">	
 		<?php include('includes/flyout_exp.php'); ?>
-		<button id="laser24" class="mybutton"><img src="img/check-button-icon1.png" />Laser <span>(24lb)</span></button> <button id="laser32" class="mybutton"><img src="img/check-button-icon1.png" />Laser <span>(32lb)</span></button> <button id="recycled100" class="mybutton"><img src="img/check-button-icon1.png" />100% Recycled</button> <button id="more-options" class="mybutton"><img src="img/check-button-icon1.png" alt="" /> More Papers</button><button id="continue" class="mybutton active">Continue</button>
-		<div class="btn_group">
-			<span>Popular Paper Options</span>
-		</div>
+		<button id="more-options" class="mybutton"><img src="img/check-button-icon1.png" alt="" /> Edit Paper Option</button><button id="continue" class="mybutton active">Looks good, continue</button>
 	</div>
 </div>
